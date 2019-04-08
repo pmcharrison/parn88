e92e8062e4286fbaea4d6f44e44a58bdbbf7a7c3
Peter M C Harrison <pmc.harrison@gmail.com>
2019-04-08 16:14:00 +0100

    Caching root_by_pc_chord

---
+++ R/parn88-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' Root by pitch-class chord
+#'
+#' This vector stores precomputed chord roots for
+#' all pitch-class chords,
+#' indexed by their encoding as defined in the "hrep" package.
+#' See \code{\link[hrep]{pc_chord}} for more details.
+#'
+#' @name root_by_pc_chord
+#' @docType data
+#' @keywords data?
+NULL
